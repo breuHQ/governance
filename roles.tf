8340cee1e3f4354ef95813ae61db7cd6cdd621e2
Saqib Akhter <101819831+SaqibAkhter99@users.noreply.github.com>
2023-01-16 14:37:22 +0500

    chore( ) adding info of the users which were present in at least one team  (#19)
    * chore() adding name of the users which were present in a team
    
    * chore() chaning hd-digital to xnaut, adding taimor to nozl group, removing mahad from xnaut and adding him to nozl group
    
    * chore() updating github repos and teams on github
    
    * chore( ) adding svt-core
    
    * chore() adding recovery email address of ahamd and faizan, adding behroz in users section and adding roles.tf file
    
    * chore() updating roles.tf file

---
+++ roles.tf
@@ -0,0 +1,18 @@
+// work in progress
+
+/*In this script, the "google_iam_member" resource is used to get the role of the 
+user from Google Workspace. The member argument is used to specify the email address 
+of the user. The "google_iam_binding" resource is used to assign the role to the 
+user on GCP. The role argument is used to set the role that should be assigned to the 
+user and the members argument is used to set the email address of the user.*/
+
+
+resource "google_iam_member" "user" {
+  role = "roles/viewer"
+  member = "user:${var.user_email}"
+}
+
+resource "google_iam_binding" "binding" {
+  role = "${google_iam_member.user.role}"
+  members = ["${google_iam_member.user.member}"]
+}
